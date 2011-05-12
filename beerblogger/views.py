90c75c4a45844ed2a8157c986ac316952040259d
Victor Fontes <vitufc@gmail.com>
2011-05-11 23:29:33 -0300

    Fazendo o locale funcionar direito no ubuntu com virtualenv

--- beerblogger/views.py
+++ beerblogger/views.py
@@ -15,7 +15,9 @@ def dateformat(value, format=u'%d/%m/%Y'):
 
 @app.template_filter('shortmonth')
 def shortmonth(value):
-    locale.setlocale(locale.LC_ALL, "pt_BR")
+    #locale.setlocale(locale.LC_ALL, "pt_BR")
+    #locale.setlocale(locale.LC_ALL, '')
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
     return value.strftime('%B')[:3].upper()
 
 
