6f68051001b539eab80ec84509264f87a142268b
Felipe Arruda Pontes <arruda@uniriotec.com>
2011-05-17 14:52:17 -0300

    Revert "Trying to put members in expecific groups"
    This reverts commit a601455a48ade856e896d8ff202dc194123f162c.

--- beerblogger/models.py
+++ beerblogger/models.py
@@ -7,21 +7,12 @@ from members import *
 
 database = peewee.Database(peewee.SqliteAdapter(), app.config['DATABASE_NAME'])
 
-class BeerGroup(peewee.Model):
-    title =     peewee.CharField()
-    gid =       peewee.CharField()
-    
-    class Meta:
-        database = database
-
-        
 class BlogEntry(peewee.Model):
     title =     peewee.CharField()
     eid =       peewee.CharField()
     link =      peewee.CharField()
 
     author_email = peewee.CharField()
-    group =     peewee.ForeignKeyField(BeerGroup)
 
     summary =   peewee.TextField() 
     tags =      peewee.CharField()
@@ -36,4 +27,3 @@ class BlogEntry(peewee.Model):
     @property
     def author(self):
         return Members().by_email(self.author_email)
-        
