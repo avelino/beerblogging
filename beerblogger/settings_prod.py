a249800eada4b589e0ddf789d3495d2ec7cec497
Victor Fontes <contato@victorfontes.com>
2011-08-21 04:45:30 -0300

    Limpando o repositório

--- beerblogger/settings_prod.py
+++
@@ -1,7 +0,0 @@
-from beerblogger.settings_common import *
-
-DEBUG = True
-
-DATABASE_NAME = '/deploy/beerblogging/beerblogger/db.sqlite'
-DUMP_FILE = '/deploy/beerblogging/beerblogger/members.yaml'
-DUMP_FOLDER = '/deploy/beerblogging/beerblogger/groups/'
--- frontend-experiment/js/bolhas.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.getElementById('ondas');
-setCanvas();
-
-function setCanvas(){
-  if (canvas.getContext){
-    ctx = canvas.getContext("2d");
-  }
-}
