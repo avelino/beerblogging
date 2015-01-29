9b411934dfab63b00359ed6706d537a9e41e1c3f
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-01-29 02:14:05 -0200

    added bug on run_external

--- manager.py
+++ manager.py
@@ -11,7 +11,7 @@ def run():
 
 def run_external():
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 def create_db():
     db.create_all()
