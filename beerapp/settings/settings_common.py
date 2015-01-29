e65595e2169804b5d33b638be54e18199e55922f
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-01-29 01:28:37 -0200

    update domain in common FEED_LINK

--- beerapp/settings/settings_common.py
+++ beerapp/settings/settings_common.py
@@ -1,7 +1,7 @@
 PAGINATION_ITEMS_PER_PAGE = 5
 
 FEED_TITLE = "BeerBlogging"
-FEED_LINK = "http://www.beerblogging.org"
+FEED_LINK = "http://www.beerblogging.io"
 FEED_AUTHOR = "BeerBlogging Team"
 FEED_DESC = "Posts from all beerbloggers"
 FEED_ITEMS = 10
