2d2adc7a5642ec7debf5360bd7b0fe800f394ec1
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-02-12 17:14:08 -0200

    Fix settings heroku

--- beerapp/settings/__init__.py
+++ beerapp/settings/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
+from settings_common import *
+
 
 ON_HEROKU = 'DATABASE_URL' in os.environ
-
-from settings_common import *
 
 if ON_HEROKU:
     from settings_prod import *
